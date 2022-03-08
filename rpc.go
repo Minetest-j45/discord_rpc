85ff68abbdc90135edf8e30a3d6a423affaedc0f
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2022-03-08 16:47:59 +0000

    Add json file to make config easier

--- rpc.go
+++ rpc.go
@@ -1,45 +1,102 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/hugolgst/rich-go/client"
 )
 
+type conf struct {
+	ClientID     string `json:"clientid"`
+	State        string `json:"state"`
+	Details      string `json:"details"`
+	LargeID      string `json:"largeid"`
+	LargeImgText string `json:"largeimgtext"`
+	SmallID      string `json:"smallid"`
+	SmallImgText string `json:"smallimgtext"`
+	Buttons      int    `json:"buttons"`
+	Button1      string `json:"button1"`
+	Button1Url   string `json:"button1url"`
+	Button2      string `json:"button2"`
+	Button2Url   string `json:"button2url"`
+}
+
+func openConf() conf {
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		os.Create("config.json")
+		openConf()
+	}
+	defer jsonFile.Close()
+
+	byteVal, _ := ioutil.ReadAll(jsonFile)
+
+	var config conf
+
+	err = json.Unmarshal(byteVal, &config)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return config
+}
+
 func main() {
-	err := client.Login("950420436020240415")
+	config := openConf()
+
+	if config.ClientID == "" {
+		fmt.Println("`clientid` is required in the config.json file")
+		os.Exit(1)
+	}
+
+	var buttonsArr []*client.Button
+	if config.Buttons > 2 {
+		fmt.Println("Too many buttons, there is a maximum of 2 buttons")
+		os.Exit(1)
+	} else if config.Buttons == 1 {
+		buttonsArr = append(buttonsArr, &client.Button{
+			Label: config.Button1,
+			Url:   config.Button1Url,
+		})
+	} else if config.Buttons == 2 {
+		buttonsArr = append(buttonsArr, &client.Button{
+			Label: config.Button1,
+			Url:   config.Button1Url,
+		})
+		buttonsArr = append(buttonsArr, &client.Button{
+			Label: config.Button2,
+			Url:   config.Button2Url,
+		})
+	}
+	err := client.Login(config.ClientID)
 	if err != nil {
 		panic(err)
 	}
 
 	now := time.Now()
 	err = client.SetActivity(client.Activity{
-		State:      "Powered by rich-go",
-		Details:    "A very difficult game",
-		LargeImage: "pfpimg",
-		LargeText:  "pfp",
-		SmallImage: "verifiedimg",
-		SmallText:  "verified",
+		State:      config.State,
+		Details:    config.Details,
+		LargeImage: config.LargeID,
+		LargeText:  config.LargeImgText,
+		SmallImage: config.SmallID,
+		SmallText:  config.SmallImgText,
 		Timestamps: &client.Timestamps{
 			Start: &now,
 		},
-		Buttons: []*client.Button{
-			&client.Button{
-				Label: "My GitHub",
-				Url:   "https://github.com/Minetest-j45",
-			},
-			&client.Button{
-				Label: "My Website",
-				Url:   "https://j1233.minetest.land/",
-			},
-		},
+		Buttons: buttonsArr,
 	})
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Running...")
-	time.Sleep(time.Hour * 1)
-	fmt.Println("Stopping...")
+	fmt.Println("Press Ctrl+C to exit, whenever you want")
+	for {
+		time.Sleep(time.Second * 5)
+	}
 }
